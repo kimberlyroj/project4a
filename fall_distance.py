02410bf8390aecea5fc17cb4723aef5d10e8e731
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-28 22:37:54 -0800

    .

--- fall_distance.py
+++ fall_distance.py
@@ -1,11 +1,12 @@
 #Author: Kimberly Rojas
-# GitHub username: kimberlyroj
-# Date: 1/28/2021
-# Description: Code that will determine the distance an object falls due to gravity in a specific time period:
+#GitHub username: kimberlyroj
+#Date: 1/28/2021
+#Description: Code that will determine the distance an object falls due to gravity in a specific time period:
+
 def fall_distance(time):#function
   gravity=9.8
   distance=(1/2)*gravity*time**2 #determine the distance
-  return round(distance, 3) #return distance
+  return round(distance,3) #return distance
 time=float(input())
 dist=fall_distance(time)
 print(dist)
