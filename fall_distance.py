2bfdac8be7da2455f686d97ebae8163f69207a26
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-01-28 22:44:31 -0800

    ./

--- fall_distance.py
+++ fall_distance.py
@@ -2,7 +2,6 @@
 #GitHub username: kimberlyroj
 #Date: 1/28/2021
 #Description: Code that will determine the distance an object falls due to gravity in a specific time period:
-
 def fall_distance(time):#function
   gravity=9.8
   distance=(1/2)*gravity*time**2 #determine the distance
